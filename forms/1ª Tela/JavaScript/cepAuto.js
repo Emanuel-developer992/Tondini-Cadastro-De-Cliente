e7eaca8a3db7c57727ccba263f5c577c58b6e552
Orion992-cpu <76693636+Orion992-cpu@users.noreply.github.com>
2021-03-21 12:57:52 -0300

    First Commit

---
+++ forms/1ª Tela/JavaScript/cepAuto.js
@@ -0,0 +1,12 @@
+$("#cep").blur(function() {
+    
+    $.getJSON("//viacep.com.br/ws/"+ $("#cep").val() +"/json/", function(dados){ 
+        
+        $("#endereco").val(dados.logradouro);
+        $("#bairro").val(dados.bairro);
+        $("#estado").val(dados.localidade);
+        $("#pais").val(dados.pais);
+        
+        
+    })
+});
